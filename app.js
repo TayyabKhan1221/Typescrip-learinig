7bb68a93f8194215627f33c54122aa84e1b66d07
TayyabKhan1221 <Tayyabkhano603@gmail.com>
2024-07-16 15:41:04 +0500

    learinig typescrip

---
+++ app.js
@@ -0,0 +1,14 @@
+// let myName: string;
+// myName = "tayyab"
+// console.log(myName);
+// let a = Number(prompt("value 1"));
+// let b = Number(prompt("value 2"));
+// let result = a + b;
+// console.log(result);
+// let userName = prompt("what is your name");
+// if (userName === null) {
+//   userName = "tayyab";
+// }
+// console.log(userName?.toUpperCase());
+var person = "tayyab";
+person = "khan";
---
+++ app.ts
@@ -0,0 +1,19 @@
+// let myName: string;
+
+// myName = "tayyab"
+// console.log(myName);
+
+// let a = Number(prompt("value 1"));
+// let b = Number(prompt("value 2"));
+
+// let result = a + b;
+// console.log(result);
+
+// let userName = prompt("what is your name");
+
+// if (userName === null) {
+//   userName = "tayyab";
+// }
+// console.log(userName?.toUpperCase());
+
+
