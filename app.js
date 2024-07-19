0c4d63e802f0615117b7c5fbd209a273bf06e5c4
TayyabKhan1221 <Tayyabkhano603@gmail.com>
2024-07-19 15:48:33 +0500

    add file

--- app.js
+++ app.js
@@ -10,5 +10,40 @@
 //   userName = "tayyab";
 // }
 // console.log(userName?.toUpperCase());
-var person = "tayyab";
-person = "khan";
+var arr = [8, "5", 5, 1];
+console.log(arr);
+arr.push("sss");
+if (false) {
+    var z = 4;
+    //use z
+    console.log(z.toFixed());
+}
+else {
+    var z = "string";
+    //use z
+    console.log(z.toUpperCase());
+}
+var myObj = {
+    name: "tayyab",
+    age: 25,
+    hobbies: [3, 5, 1],
+    test: true,
+};
+console.log(myObj.hobbies[2].toFixed(5));
+// narrowing
+var myAge;
+myAge = "shehzad";
+console.log(myAge.toUpperCase());
+myAge = 56;
+console.log(myAge.toFixed());
+myAge = false;
+var userName = "tayyab";
+var userAge = 58;
+var isUserActive = true;
+var userFavDishes = [
+    { name: "Biryani", location: "Naseeb" }
+];
+var userParentInfo = {
+    fatherName: "ABC",
+    motherName: "ABC",
+};
--- app.ts
+++ app.ts
@@ -16,4 +16,33 @@
 // }
 // console.log(userName?.toUpperCase());
 
+let arr = [8, "5", 5, 1];
+console.log(arr);
+
+arr.push("sss");
+
+if (false) {
+  let z = 4;
+  //use z
+  console.log(z.toFixed());
+} else {
+  let z = "string";
+  //use z
+  console.log(z.toUpperCase());
+}
+
+const myObj = {
+  name: "tayyab",
+  age: 25,
+  hobbies: [3, 5, 1],
+  test: true,
+};
+console.log(myObj.hobbies[2].toFixed(5));
+// narrowing
+let myAge: string | number | boolean;
+myAge = "shehzad";
+console.log(myAge.toUpperCase());
+myAge = 56;
+console.log(myAge.toFixed());
+myAge = false;
 
